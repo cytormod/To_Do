6abe91cae3f773c92d8c89e392fc7ed1015fbe1a
cytormod <sahil.gopani5@gmail.com>
2025-03-29 14:47:42 +0100

    Create Task Button

--- TODO-1/script2.js
+++ TODO-1/script2.js
@@ -1,9 +1,10 @@
 "use strict";
 // & Today's task area
+const taskCreate = document.querySelector(".created_task");
 const btnNewTaskEvent = document.querySelector(".btnNewTask");
 
 // & Modal Window
-const blurryBackground = document.querySelector(".flex2");
+const blurryBackground = document.querySelector(".felx2");
 const modalMain = document.querySelector(".modal-main");
 const mdCloseBtn = document.querySelector(".md_closeBtn");
 const mdtaskName = document.querySelector("#md_task-name");
@@ -12,9 +13,7 @@ const todaysDate = document.querySelector("#today_Date");
 const mdTaskLists = document.querySelector(".md_taskLists");
 const mdSelectLists = document.querySelector(".md_selectLists");
 const mdDueDate = document.querySelector(".md_dueDate");
-const mdDeleteSave = document.querySelector(".md_delete-save");
-
-console.log(description);
+const createTask = document.querySelector(".md_delete-save");
 
 // & Menu - Task
 const subTasks = document.querySelector(".sub-tasks");
@@ -40,5 +39,44 @@ const subLists = document.querySelector(".sub-lists");
 const indetailTaskEve = document.querySelector(".indetailTask");
 
 // & Overview
-const selectedTask = document.querySelector(".selected-task");
 const currentTasksList = document.querySelector(".current-tasks-list");
+const selectedTask = document.querySelector(".selected-task");
+
+// ^ Functions
+
+function modalWindow(value, grayscale) {
+  todaysDate.value = new Date().toISOString().split("T")[0];
+  modalMain.style.display = `${value}`;
+  blurryBackground.style.filter = `${grayscale}`;
+}
+
+// ^ CODE
+
+btnNewTaskEvent.addEventListener("click", () => {
+  modalWindow("block", "blur(1.3px)" + "grayscale(100%)");
+});
+
+mdCloseBtn.addEventListener("click", () => {
+  modalWindow("none", "none");
+});
+
+createTask.addEventListener("click", () => {
+  taskCreate.innerHTML += `
+  <div class="overviewTasks"> 
+          <li>
+                      <input type="checkbox" name="1st-Task" id="generalTaskId" />
+                      <label for="1st-Task">${mdtaskName.value}</label>
+                      <br />
+                    </li>
+                    <div class="overviewNaming">
+                      <h5>Overview</h5>
+                      <h5>Delete task</h5>
+                    </div>
+                  </div>
+                  </div>
+      `;
+  console.log(taskCreate);
+  console.log(mdtaskName.value, description.value, todaysDate.value);
+  console.log(mdTaskLists.lastElementChild.value);
+  modalWindow("none", "none");
+});
